a28e4d4d3a838ac87dbb311c82afff2d7e3ac221
Dora <dg457@njit.edu>
2017-10-11 22:21:40 -0400

    added dol

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ public function printCount($books) {
 public function stringHeredoc ($dog){
 	echo '<h1>String Heredoc function</h1>';
 	echo 'Print: ';
-	echo 'This array uses <<< as heredoc: '; $dog
+	echo 'This array uses <<< as heredoc: ' . $dog;
 	echo $dog;
 	echo '<hr>';
 	
