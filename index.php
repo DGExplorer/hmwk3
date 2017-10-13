ae683c25d47165a7a05d6a1455246f5ad1a602fc
Dora <dg457@njit.edu>
2017-10-12 21:20:42 -0400

    added nom

--- index.php
+++ index.php
@@ -11,18 +11,27 @@ $dog = <<<WALKERS
 'There are not enough treats for canines!'
 WALKERS;
 $comment = "This is a\nmulti-line string.";
-
+$odd = 16;
+$name = 'President Abraham Lincoln';
+$names = array('first' => 'Abraham', 'last' => 'Lincoln');
 
 $obj->printString($textInput);
 $obj->stringLength($textInput);
 $obj->arraylength($num);
 $obj->printCount($books);
-$obj->stringHeredoc ($dog);
+$obj->stringNowdoc ($dog);
 $obj->arrayAddValue ($num);
 $obj->stringMultiLine ($comment);
-$obj->arrayAddAllValues ($sum)
+$obj->arrayAddAllValues ($sum);
+$obj->stringSqRoot ($odd);
+$obj->arraySetaValue ($num);
+$obj->stringSpace ($name);
+$obj->arraySpecKey ($names);
+$obj->stringSubstring ($name);
+$obj->arrayFill ($nom);
  
 class main {
+
   public function __construct() {
   
   echo 'Hello PHP!';
@@ -64,10 +73,10 @@ public function printCount($books) {
 	
 }
 
-public function stringHeredoc ($dog){
-	echo '<h1>String Heredoc function</h1>';
+public function stringNowdoc ($dog){
+	echo '<h1>String Nowdoc function</h1>';
 	echo 'Print: ';
-	echo 'This array uses heredoc: ' . $dog;
+	echo 'This array allows you to use a multi-line string: ' . $dog;
 	echo '<hr>';
 	
 }
@@ -75,7 +84,7 @@ public function stringHeredoc ($dog){
 public function arrayAddValue ($num) {
 	echo '<h1>Array Add Value function</h1>';
 	echo 'Print: ';
-	echo 'This array uses brackets: ' . $num [] = '10';
+	echo 'This array adds a value at the end: ' . $num [] = '10';
 	echo '<hr>';
 }
 
@@ -87,12 +96,60 @@ public function stringMultiLine ($comment) {
 }
 	
 public function arrayAddAllValues ($sum)   {
-	echo '<h1>Array  function</h1>';
+	echo '<h1>Array Add All Values function</h1>';
 	echo 'Print: ';
-	echo 'This array adds all values: ' . $sum;
+	echo 'This array adds all values to: ' . $sum;
 	echo '<hr>';
+	
 }
 
+public function stringSqRoot ($odd) {
+	echo '<h1>String Square Root function</h1>';
+	echo 'Print: ';
+	echo 'This string performs the square root of 16: ' . $root = sqrt($odd);
+	echo '<hr>';
+	
+}
+
+public function arraySetaValue ($num) {
+	echo '<h1>Array Set a Value at Specific Index function</h1>';
+	echo 'Print: ';
+	echo 'This array will replace the first index of the array: ' . $num[0] = '1';
+	echo '<hr>';
+	
+}
+
+public function stringSpace ($name) {
+	echo '<h1>String Searches for Space function</h1>';
+	echo 'Print: ';
+	echo 'This string searches for space: ' . $i = strpos($name,' ');
+	echo '<hr>';
+
+}
+
+public function arraySpecKey ($names) {
+	echo '<h1>Array Specified key function</h1>';
+	echo 'Print: ';
+	echo 'This array will get a value at a specified key: ' . $first_name = $names['first'];
+	echo '<hr>';
+	
+}
+
+public function stringSubstring ($name) {
+	echo '<h1>String Searches for a Substring function</h1>';
+	echo 'Print: ';
+	echo 'This string searches for a Substring: ' . $i = strpos($name,'Abraham');
+	echo '<hr>';
+
+}
+
+public function arrayFill ($nom) {
+	echo '<h1>Array Range Values function</h1>';
+	echo 'Print: ';
+	echo 'This array has a range of values: ' . $nom = range(2, 2);
+	echo '<hr>';
+
+}
 	
 public function __destruct() {
 
